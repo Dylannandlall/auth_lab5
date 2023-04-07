109f1605d1120a864fc120177efddf3400fdff2d
Dylan Nandlall <dan6705@rit.edu>
2023-04-07 06:04:34 -0400

    fixed grant_type

--- authentication_server/oauth_token.py
+++ authentication_server/oauth_token.py
@@ -6,13 +6,21 @@ def get_token():
     user = "testclient"
     password = "testpass"
     url = "http://localhost:8080/token.php"
+    type = "grant_type=client_credentials"
+    bashCommand = f"curl -u {user}:{password} {url} -d {type}"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output)
 
+    # headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/plain'}
 
-    # bashCommand = f"curl -u {user}:{password} {url} -d 'grant_type=client_credentials'"
-    # bashCommand = "curl -u testclient:testpass http://192.168.207.34:8080/token.php -d 'grant_type=client_credentials'"
-    result = subprocess.run(['curl', '-u testclient:testpass', 'http://192.168.207.34:8080/token.php', "-d 'grant_type=client_credentials'"], stdout=subprocess.PIPE, text=True)
-    print(result)
-   
+    # json_data = {
+    #     'grant_type': 'client_credentials'
+    # }
+    # json_data = json.dumps(json_data)
+    # requests_data = requests.post("http://192.168.207.34:5001", data=json_data, auth=("testclient", "testpass"), headers=headers)
+    # print(requests_data.raise_for_status())
+
 
 if __name__ == "__main__":
     get_token()
