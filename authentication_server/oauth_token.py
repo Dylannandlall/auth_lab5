2c3a7b33d7ded8d93fb63ccbeb1a5ff0e03a8722
Dylan Nandlall <dan6705@rit.edu>
2023-04-07 05:37:30 -0400

    added headers

---
+++ authentication_server/oauth_token.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+def get_token():
+    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/plain'}
+
+    json_data = {
+        'grant_type': 'client_credentials'
+    }
+    requests_data = requests.post("http://192.168.207.34:5001", data=json_data, auth=("testclient", "testpass"), headers=headers).json()
+    print(requests_data)
+
+
+if __name__ == "__main__":
+    get_token()
