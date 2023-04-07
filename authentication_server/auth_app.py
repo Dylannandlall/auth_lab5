afbe1c78e09154a0d253fd5c187353186b148c3d
Dylan Nandlall <dan6705@rit.edu>
2023-04-06 21:25:05 -0400

    encryption and decryption with password works between client and auth_server

--- authentication_server/auth_app.py
+++ authentication_server/auth_app.py
@@ -1,8 +1,18 @@
+import requests
+import json
+import rsa
+import base64
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+OAUTH_SERVER = "http://192.168.207.34:5000"
+SECRET_KEY = "abcdefg"
 
 @app.route("/login", methods=['POST'])
 def login():
@@ -12,7 +22,43 @@ def login():
     username = request_data['username']
     password = request_data['password']
 
-    return jsonify(auth="success")
+    data = {
+            'username': username, 
+            'password': password
+        }
+    json_data = json.dumps(data)
+
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+    # response = requests.post(f"{OAUTH_SERVER}/login", data=json_data, headers=headers)
+
+    # if response["status"] == "invalid":
+    #     return jsonify(auth="fail")
+
+    # if response["status"] == "valid":
+    #     encrypted_token = encrypt(response["oauth_token"], SECRET_KEY)
+    # json_response = jsonify(auth="success", token=encrypted_token)
+
+    encrypted_token = (encrypt("This is a token!", string_kdf(SECRET_KEY)))
+    json_response = {"auth":"success", "token":encrypted_token}
+    json_string = json.dumps(json_response)
+    return jsonify(message=(encrypt(json_string, string_kdf(password))))
+
+def encrypt(message, key):
+    fernet = Fernet(key)
+    encrypted_message = fernet.encrypt(message.encode())
+    return encrypted_message.decode()
+
+def string_kdf(password):
+    salt = b'1111'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000)
+    
+    return base64.urlsafe_b64encode(kdf.derive(bytes(password, 'utf-8')))
+
 
 if __name__ == "__main__":
     app.run("localhost", port=5000, debug=True)
---
+++ authentication_server/test.py
@@ -0,0 +1,59 @@
+import rsa
+import base64
+import hashlib
+import os
+import json
+import cryptography.exceptions
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
+
+
+def main():
+    # publicKey, privateKey = rsa.newkeys(512)
+    publicKey = "hello"
+
+    message = { "name":"John", "age":30, "city":"New York"}
+    message = json.dumps(message)
+    
+    salt = b"1111"
+
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000)
+    
+    key = base64.urlsafe_b64encode(kdf.derive(bytes(publicKey, 'utf-8')))
+    # key = kdf.derive(b"password")
+
+    salt = b"1111"
+    kdf2 = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000)
+    
+    try:
+        kdf2.verify((bytes(publicKey, 'utf-8')), base64.urlsafe_b64decode(key))
+    except cryptography.exceptions.InvalidKey:
+        print("Key is not Valid!")
+    else:
+        print("Key is valid!")
+
+    fernet = Fernet(key)
+
+
+    encrypted_message = fernet.encrypt(message.encode())
+    print(encrypted_message.decode())
+    # string_encrypted_message = base64.encodebytes(encrypted_message)
+    # # print(encrypted_message)
+    # print(string_encrypted_message)
+    # string_encrypted_message = string_encrypted_message.decode()
+    # print(string_encrypted_message)
+    # string_encrypted_message = string_encrypted_message.encode()
+    # print(string_encrypted_message)
+    # # if encrypted_message == base64.decodebytes(string_encrypted_message):
+    # #     print("True!")
+if __name__ == "__main__":
+    main()
