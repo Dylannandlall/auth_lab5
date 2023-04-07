c45d08ced0dd9bbdec39f8ea9f5015802d9211f1
Dylan Nandlall <dan6705@rit.edu>
2023-04-07 05:54:58 -0400

    fixed python curl again

--- authentication_server/oauth_token.py
+++ authentication_server/oauth_token.py
@@ -6,10 +6,10 @@ def get_token():
     user = "testclient"
     password = "testpass"
     url = "http://localhost:8080/token.php"
-    type = "'grant_type=client_credentials'"
 
-    bashCommand = f"curl -u {user}:{password} {url} -d {type}"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+
+    bashCommand = f"curl -u {user}:{password} {url} -d 'grant_type=client_credentials'"
+    process = subprocess.run(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     print(output)
    
