5188667cf9d75bca6903fc6adbdd37b9469bc9b4
Dylan Nandlall <dan6705@rit.edu>
2023-04-07 05:51:23 -0400

    added bash curl command

--- authentication_server/oauth_token.py
+++ authentication_server/oauth_token.py
@@ -1,16 +1,18 @@
 import requests
 import json
+import subprocess
 
 def get_token():
-    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/plain'}
+    user = "testclient"
+    password = "testpass"
+    url = "http://localhost:8080/token.php"
+    type = "'grant_type=client_credentials'"
 
-    json_data = {
-        'grant_type': 'client_credentials'
-    }
-    json_data = json.dumps(json_data)
-    requests_data = requests.post("http://192.168.207.34:5001", data=json_data, auth=("testclient", "testpass"), headers=headers).json()
-    print(requests_data)
-
+    bashCommand = f"curl -u {user}:{password} {url} -d {type}"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output)
+   
 
 if __name__ == "__main__":
     get_token()
